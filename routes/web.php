f47a18336d4e8b66b0722d9ba38c9f9d48ac3171
thoriqfathu <thoriq771@gmail.com>
2025-10-17 01:40:44 +0700

    repush

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
-Route::get('/', [KlaimController::class, 'index'])->name('index');
+// Route::get('/', [KlaimController::class, 'index'])->name('index');
 Route::prefix('monitoring')->name('monitoring.')->middleware('auth.session')->group(function () {
 
     Route::prefix('sinkron-sep')->name('sinkron_sep.')->group(function () {
